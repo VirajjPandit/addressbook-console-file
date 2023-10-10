0537484d95e35805f5e634f0ea587295db4f0d23
VirajjPandit <146913890+VirajjPandit@users.noreply.github.com>
2023-10-10 18:07:43 +0530

    Update UserOptions.java

--- src/User_Management/UserOptions.java
+++ src/User_Management/UserOptions.java
@@ -1,12 +1,9 @@
 package User_Management;
-
-public class UserOptions {
-	
-
+    public class UserOptions {
 	public static final int ADD_USER=1;
-public static final int EDIT_USER=2;
-public static final int DELETE_USER=3;
-public static final int SEARCH_USER=4;
-public static final int QUIT=5;
+        public static final int EDIT_USER=2;
+        public static final int DELETE_USER=3;
+        public static final int SEARCH_USER=4;
+        public static final int QUIT=5;
+} 
 
-}
